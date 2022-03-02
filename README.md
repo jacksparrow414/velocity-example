ba775751b4a39c5065b50603d9b8e9e467444bca
Serendipity <jacksparrow414@163.com>
2022-03-02 19:58:59 +0800

    使用VelocityEngine

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 - 写页面应该看的[文档](https://velocity.apache.org/engine/2.3/user-guide.html)
 - 写Java代码应该看的[文档](https://velocity.apache.org/engine/2.3/developer-guide.html#developer-guide-contents)
-- 
--- src/main/java/com/demo/velocity/emails/SendEmailService.java
+++ src/main/java/com/demo/velocity/emails/SendEmailService.java
@@ -2,33 +2,46 @@ package com.demo.velocity.emails;
 
 import com.demo.velocity.entity.User;
 import lombok.extern.java.Log;
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
 
+/**
+ * 使用 VelocityEngine 出现 Unable to find resource解决方案
+ * https://www.jianshu.com/p/bc2076c4578d
+ * https://stackoverflow.com/questions/9051413/unable-to-find-velocity-template-resources
+ */
 @Service
 @Log
 public class SendEmailService {
-
+    
+    private static final VelocityEngine ve = new VelocityEngine();
+    
     @PostConstruct
     public void initVelocity() {
-        Velocity.init();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        ve.init();
     }
+    
 
     public boolean sendRegisterSuccessEmail() {
         VelocityContext context = new VelocityContext();
         context.put("user", User.builder().name("jack").build());
-        StringWriter w = new StringWriter();
-        Velocity.mergeTemplate(obtainRealTemplatePath("registerSuccess"), Charset.defaultCharset().name(), context, w);
-        log.info(w.toString());
+        Template t = ve.getTemplate(obtainRealTemplatePath("registerSuccess"));
+        StringWriter writer = new StringWriter();
+        t.merge( context, writer );
+        log.info(writer.toString());
         return true;
     }
 
     private String obtainRealTemplatePath(String templateName) {
-        return getClass().getClassLoader().getResource("templates/emails/" + templateName + ".vm").getPath();
+        return "templates/emails/" + templateName + ".vm";
     }
 }
